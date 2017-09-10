c166a9ebce84f05b0045c0c18bcef5b33ab3a52e
Mattias Lasersköld <mattias.l.sk@gmail.com>
2017-09-10 20:30:33 +0200

    Created basic r2d2 like voice

---
+++ samples/r2/raw/translate.sh
@@ -0,0 +1,81 @@
+
+
+function apa {
+    echo copying $1.mp3 ../$2.wav
+    ffmpeg -y -i $1.mp3 ../$2.wav
+}
+
+
+apa 1 a
+apa 2 e
+apa 27 i
+apa 5 o
+apa 4 u
+apa 6 å
+apa 8 ä
+apa 31 ö
+apa 9 y
+
+apa 32 t
+apa 10 b
+apa 19 d
+
+apa 11 g
+apa 17 h
+
+apa 20 p
+apa h2 q
+apa 21 k
+
+apa 12 l
+apa 14 m
+apa 13 n
+apa 24 r
+apa 7 v
+
+apa h1 c
+apa 26 s
+apa 28 z
+apa 29 f
+
+apa 23 j
+
+apa 36 x
+
+
+exit;
+
+aeiouy
+åäö
+
+(bip-liknande) tbdghkpq
+(väsande) cszf
+(brummande) lmnrv
+jlx
+
+
+
+siffrorna
+mysiga
+1:a 2:e 3 9:y 12:l 13:n 14:m
+23:j 24:r
+(flängande) 26:s 27:i 29:f
+
+hmmande
+4:u 5:o 6:å 8:ä
+(med pip) 33 34 35 36:x 37 38
+
+konsonantliknande/passande
+7:v 11:g 10:b
+30 h5
+
+(korta bip) 17:h 18 18b 19:d 20:p 20b 32:t h1:c h2 h3 h4 
+(klick) 21:k 
+
+visslande
+15 16 22
+(lite surrande) 25 28:z
+
+
+längre
+31:ö 
